89374e1adcbdf02afd7b59509d3cfccafe48d12e
TheyllorDev <theyllormenezes@hotmail.com>
2022-09-29 08:57:37 -0300

    ProjetoClass

--- Projeto1.py
+++ Projeto1.py
@@ -1,5 +1,4 @@
 from ProjetoClass import *
-
 # from abc import ABC, abstractmethod 
 from datetime import date
 import time
@@ -7,10 +6,10 @@ from time import sleep
 import sys
 
 print ()
-simbolo = "✿ " * 25
-texto = "✿   Bem vindo(a) á biblioteca Livrinho Bom!!!   ✿"
+simbolo = "📚 " * 17
+texto = "📚   Bem vindo(a) á biblioteca Livrinho Bom!!!  📚"
 print (simbolo)
-print (texto.center (50))
+print (texto)
 print (simbolo)
 
 while True:
@@ -25,18 +24,18 @@ while True:
         print ("[2] Cadastre-se")
         print ("[3] Esqueceu sua senha?")
         print ("[4] Voltar")
-        option = input (">> ")
+        loginOption = input (">> ")
 
-        if option == "1":
+        if loginOption == "1":
             email = input ("Digite seu e-mail:")
             senha = input ("Digite sua senha:")
 
-        elif option == "2":
+        elif loginOption == "2":
             print ()
             nome = input ("Digite seu nome completo: \n")
             while True:
-                idade = input ("Digite sua data de nascimento: \n")
-                if not idade.isdigit():
+                data = input ("Digite sua data de nascimento: \n")
+                if not data.isdigit():
                     print ("Digite apenas números.")
                     continue
                 email = input ("Digite seu e-mail: \n")
@@ -44,21 +43,23 @@ while True:
                 print ("Sua senha deve ter ao menos 8 dígitos, pelo menos 1 letra maiúscula e 1 caracter especial!")
                 print ("Digite uma senha válida:")
                 senha = input (">> ")
+                print ()
 
                 bio = Biblioteca ()
-                bio.inserirCliente (nome.title(), idade, email, senha)
+                bio.inserirCliente (nome.title(), data, email, senha)
                 
                 sleep (2)
                 print ()
                 print ("Cadastro realizado com sucesso!")
                 print ()
+
                 break
             break
 
-        elif option == "3":
+        elif loginOption == "3":
             pass
         
-        elif option == "4":
+        elif loginOption == "4":
             continue
 
         else:
@@ -66,12 +67,33 @@ while True:
             continue
 
     elif escolha == "2":
+        print ()
+        print ("[1] Ver acervo")
+        print ("[2] Cadastrar novo livro")
+        print ("[3] Voltar")
+        livroOption = input (">> ")
+
+        if livroOption == "1":
+            
+            break
+        elif livroOption == "2":
+            
+            break
+        elif livroOption == "3":
+            continue
+        else:
+            print ("Opção incorreta!")
+            continue
+
         bio = Biblioteca()
         bio.printarLivros()
 
     else:
         print ("Opçao incorreta!")
         continue
+    
+cursor.close()
+conexao.close ()
 
 # bio = Biblioteca ()
 
--- ProjetoClass.py
+++ ProjetoClass.py
@@ -1,9 +1,24 @@
+import sqlite3
 from abc import ABC, abstractmethod 
 import re
 from datetime import date
 import time
 from time import sleep
 import sys
+import datetime
+import random
+
+conexao = sqlite3.connect ("biblioteca.db")
+cursor = conexao.cursor ()
+
+cursor.execute ("SELECT * FROM cadastro")
+for linha in cursor.fetchall ():
+    print (linha)
+
+# cursor.execute ("DELETE FROM cadastro")
+# conexao.commit ()
+# cursor.execute ("DELETE FROM conta")
+# conexao.commit ()
 
 def escrever (texto):
     for letra in texto:
@@ -24,42 +39,70 @@ class Pessoa:
         data = str (data)
         data = re.sub (r"[^0-9]","", data)
         listaData = list (data)
-        print (listaData)
         dia = int (f"{listaData.pop(0)}{listaData.pop(0)}")
         mes = int (f"{listaData.pop(0)}{listaData.pop(0)}")
         ano = int (f"{listaData.pop(0)}{listaData.pop(0)}{listaData.pop(0)}{listaData.pop(0)}")
-        dataCompleta = f"{dia}/{mes}/{ano}"
+        # dataCompleta = (f"{dia}/{mes}/{ano}")
         idade = self.dataNascimento (date (ano, mes, dia))
-        print (dataCompleta)
         return idade
+
+    def data_Completa (self,data):
+        datalist=[]
+        for i in data:
+            datalist.append(i)
+        for i in range(len(datalist)):
+            if i == 2:
+                datalist[2]='/'+ datalist[2]
+            if i == 4:
+                datalist[4]='/'+ datalist[4]
+        data = ''.join(datalist)
+        return data
 
 class Cliente (Pessoa):
     def __init__ (self, nome, dataCompleta, email, senha) -> None:
         super().__init__ (nome)
+        self.dataCompleta = dataCompleta
+        self.idade = self.calculadora (self.dataCompleta)
+        # self.data = data
+        self.data = self.data_Completa (dataCompleta)
+        self.matricula = self.matriculaFunc ()
         self.email = email
         self.senha = senha
-        self.dataCompleta = dataCompleta
-        self.idade = self.calculadora(self.dataCompleta)
+
+    def matriculaFunc (self):
+        ano = datetime.date.today().strftime('%Y')
+        cont = random.randint(0000, 9999)
+        matricula = f"{ano}.{cont}"
+        return matricula
+
 class Livro:
-    def __init__(self, nomeLivro, autor, dataPublicar,faixaEt) -> None:
-        self.nomeLivro = nomeLivro
+    def __init__(self, titulo, autor, quantidade, genero, faixa_etaria, npaginas, data_de_edicao) -> None:
+        self.titulo = titulo
         self.autor = autor
-        self.dataPublicar = dataPublicar
-        self.faixaEt = faixaEt
+        self.quantidade = quantidade
+        self.genero = genero
+        self.faixa_etaria = faixa_etaria
+        self.npaginas = npaginas
+        self.data_de_edicao = data_de_edicao
 
 class Biblioteca:
     def __init__(self) -> None:
-        self.membro = []
+        self.membro = ''
         self.livros = []
-    def inserirCliente(self, nome, dataCompleta, email, senha):
-        self.membro.append (Cliente (nome, dataCompleta, email, senha))
-    def printarClientes(self):
-        for i in self.membro:
-            print(f"Membro: {i.nome} \nIdade: {i.idade} \nData de Nascimento: {i.dataCompleta}")
-    def inserirLivros (self, nomeLivro, autor, dataPublicar, faixaEt):
-        self.livros.append (Livro (nomeLivro, autor, dataPublicar, faixaEt))
+
+    def inserirCliente (self, nome, dataCompleta, email, senha):
+        self.membro = (Cliente (nome, dataCompleta, email, senha))
+        cursor.execute (f"INSERT INTO cadastro (nome, data_de_aniversario, matricula, email, senha) VALUES (?, ?, ?, ?, ?)", (nome, self.membro.idade, self.membro.matricula, email, senha))
+        conexao.commit ()
+        print(f"Membro: {self.membro.nome} \nData de nascimento: {self.membro.data} \nIdade: {self.membro.idade} \nMatrícula: {self.membro.matricula} \nE-mail: {self.membro.email} \nSenha: {len (self.membro.senha) * '*'}")
+
+    def inserirLivros (self, titulo, autor, quantidade, genero, faixa_etaria, npaginas, data_de_edicao):
+        self.livros.append (Livro (titulo, autor, quantidade, genero, faixa_etaria, npaginas, data_de_edicao))
+        cursor.execute (f"INSERT INTO livros_na_estante (titulo, autor, quantidade, genero, faixa_etaria, npaginas, data_de_edicao) VALUES (?, ?, ?, ?, ?, ?, ?)", (titulo, autor, quantidade, genero, faixa_etaria, npaginas, data_de_edicao))
+        conexao.commit ()
     def printarLivros (self):
         for i in self.livros:
-            print (f"Livro: {i.nomeLivro} \nAutor: {i.autor} \nAno de publicação: {i.dataPublicar} \nClassificação etária: {i.faixaEt}")
-    def emprestarLivro (self, valor):
+            print (f"Livro: {i.titulo} \nAutor: {i.autor} \nAno de publicação: {i.dataPublicar} \nClassificação etária: {i.faixaEt}")
+
+    def emprestarLivro (self):
         pass

95fdd41f3157b25e83b3baf9d0f449bd6c49c96a
TheyllorDev <theyllormenezes@hotmail.com>
2022-10-05 17:25:21 -0300

    Projeto

--- Projeto1.py
+++ Projeto1.py
@@ -18,152 +18,109 @@ while True:
     print ("[3] Exit")
     escolha = input (">> ")
 
-    if escolha == "1":
-        while True:
-            print ()
-            print ("[1] Login")
-            print ("[2] Cadastre-se")
-            print ("[3] Esqueceu sua senha?")
-            print ("[4] Voltar")
-            loginOption = input (">> ")
-
-            if loginOption == "1":
+    match escolha:
+        case "1":
+            while True:
                 print ()
-                bio = Biblioteca ()
-                bio.loginCliente ()
-                while True:
-                    print ()
-                    print ("[1] Emprestar novo livro")
-                    print ("[2] Ver livros emprestados")
-                    print ("[3] Devolver livro(s) emprestado(s)")
-                    print ("[4] Sair da conta")
-                    logon = input (">> ")
+                print ("[1] Login")
+                print ("[2] Cadastre-se")
+                print ("[3] Esqueceu sua senha?")
+                print ("[4] Voltar")
+                loginOption = input (">> ")
 
-                    if logon == "1":
+                match loginOption:
+                    case "1":
                         print ()
-                        sleep (2)
-                        bio.printarLivros ()
-                        bio.emprestarLivro ()
-                        print()
-                        print ("Mais alguma coisa?")
-                        print ("[1] Sim")
-                        print ("[2] Não")
-                        mais = input (">> ")
-                        if mais == "1":
-                            continue
-                        else:
-                            break
-                                                                        
-                    elif logon == "2":
-                        print()
-                        bio.verLivroEmprestado ()
-                        sleep (2)
-                        continue
-                    elif logon == "3":
-                        pass
+                        bio = Biblioteca ()
+                        bio.loginCliente ()
+                        while True:
+                            print ()
+                            print ("[1] Emprestar novo livro")
+                            print ("[2] Ver livros emprestados")
+                            print ("[3] Devolver livro(s) emprestado(s)")
+                            print ("[4] Sair da conta")
+                            print ("[5] Deletar sua conta")
+                            logon = input (">> ")
 
-                    elif logon == "4":
+                            match logon:
+                                case "1":
+                                    
+                                    print ()
+                                    sleep (2)
+                                    bio.printarLivros ()
+                                    bio.emprestarLivro ()
+                                    print()
+                                    print ("Mais alguma coisa?")
+                                    print ("[1] Sim")
+                                    print ("[2] Não")
+                                    mais = input (">> ")
+                                    if mais == "1":
+                                        continue
+                                    else:
+                                        break
+                                                                                    
+                                case "2":
+                                    bio.verLivroEmprestado ()
+                                    continue
+
+                                case "3":
+                                    bio.devolverLivro ()
+
+                                case "4":
+                                    break
+
+                                case "5":
+                                    bio.deletarConta()
+                                    break
+                                case _:
+                                    print ("Opção incorreta!")
+                                    continue
+                            continue
+
+                    case "2":
+                        bio.inserirCliente ()
+                        continue
+
+                    case "3":
+                        bio.recuperarSenha ()
                         break
 
-                    else:
+                    case "4":
+                        break
+
+                    case _:
                         print ("Opção incorreta!")
                         continue
+
+        case "2":
+            print ()
+            print ("[1] Ver acervo")
+            print ("[2] Cadastrar novo livro")
+            print ("[3] Voltar")
+            livroOption = input (">> ")
+
+            match livroOption:
+                case "1":
+                    bio = Biblioteca ()
+                    bio.printarLivros ()
                     continue
 
-            elif loginOption == "2":
-                print ()
-                nome = input ("Digite seu nome completo: \n")
-                print()
-                print ("Digite sua data de nascimento:")
-                while True:
-                    data = input ("(Apenas números.) \n")
-                    if not data.isdigit():
-                        continue
-                    else:
-                        break
-                print ()
-                email = input ("Digite seu e-mail: \n")
-                print ()
-                print ("Sua senha deve ter ao menos 8 caracteres, 1 número, 1 caractere maiúsculo e 1 caractere especial.")
-                while True:
-                    senha = input ("Digite sua senha: \n")
-                    if len (senha) < 7:
-                        print ("A senha deve ter ao menos 8 caracteres:")
-                    elif senha.islower():
-                        print ("A senha deve ter ao menos 1 caractere maiúsculo:")
-                    elif senha.isalpha():
-                        print ("A senha deve ter ao menos 1 número:")
-                    elif senha.isalnum():
-                        print ("A senha deve ter ao menos 1 caractere especial:")
-                    else:
-                        break
-                print ()
-                bio = Biblioteca ()
-                bio.inserirCliente (nome.title(), data, email, senha)
-                
-                sleep (2)
-                print ()
-                print ("Cadastro realizado com sucesso!")
-                continue
+                case "2":
+                    bio.inserirLivros ()
+                    bio.printarLivros ()
+                    continue
 
-            elif loginOption == "3":
+                case "3":
+                    continue
+                case _:
+                    print ("Opção incorreta!")
+                    continue
 
-                recuperar = Biblioteca ()
-                recuperar.recuperarSenha ()
-                break
-
-            elif loginOption == "4":
-                break
-
-            else:
-                print ("Opção incorreta!")
-                continue
-
-    elif escolha == "2":
-        print ()
-        print ("[1] Ver acervo")
-        print ("[2] Cadastrar novo livro")
-        print ("[3] Voltar")
-        livroOption = input (">> ")
-
-        if livroOption == "1":
-            bio = Biblioteca ()
-            bio.printarLivros ()
+        case "3":
             break
 
-        elif livroOption == "2":
-            print ()
-            nomeLivro = input ("Digite o título do livro: \n")
-            autor = input ("Digite o nome do autor: \n")
-            quantidade_disponivel = int (input ("Quantidade de exemplares disponíveis: \n"))
-            genero = input ("Á que gênero pertecence o seu livro: \n")
-            faixaEt = input ("Faixa etária do livro: \n")
-            Npaginas = int (input ("Número de páginas: \n"))
-            data_de_edicao = input ("Data de publicação dessa edição: \n")
-            print ()
-
-            bio = Biblioteca ()
-            bio.inserirLivros (nomeLivro.title(), autor, quantidade_disponivel, genero, faixaEt, Npaginas, data_de_edicao)
-            
-            sleep (2)
-            print ()
-            print ("Cadastro realizado com sucesso!")
-            print ()
-
-            bio.printarLivros ()
-            break
-
-        elif livroOption == "3":
-            continue
-        else:
-            print ("Opção incorreta!")
-            continue
-
-    elif escolha == "3":
-        break
-
-    else:
-        print ("Opção incorreta.")
+        case _:
+            print ("Opção incorreta.")
     
 cursor.close()
 conexao.close ()
--- ProjetoClass.py
+++ ProjetoClass.py
@@ -1,5 +1,4 @@
 import sqlite3
-from abc import ABC, abstractmethod 
 import re
 from datetime import date
 import time
@@ -22,7 +21,7 @@ def escrever (texto):
     for letra in texto:
         sys.stdout.write(letra)
         sys.stdout.flush ()
-        time.sleep (0.04)
+        time.sleep (0.02)
 
 class Pessoa:
     def __init__ (self, nome) -> None:
@@ -43,16 +42,16 @@ class Pessoa:
         idade = self.dataNascimento (date (ano, mes, dia))
         return idade
 
-    def data_Completa (self,data):
-        datalist=[]
+    def data_Completa (self, data):
+        datalist = []
         for i in data:
-            datalist.append(i)
-        for i in range(len(datalist)):
+            datalist.append (i)
+        for i in range (len (datalist)):
             if i == 2:
-                datalist[2]='/'+ datalist[2]
+                datalist [2] = '/' + datalist [2]
             if i == 4:
-                datalist[4]='/'+ datalist[4]
-        data = ''.join(datalist)
+                datalist [4] = '/' + datalist [4]
+        data = ''.join (datalist)
         return data
 
 class Cliente (Pessoa):
@@ -67,7 +66,7 @@ class Cliente (Pessoa):
 
     def matriculaFunc (self):
         ano = datetime.date.today().strftime('%Y')
-        cont = random.randint(0000, 9999)
+        cont = random.randint (0000, 9999)
         matricula = f"{ano}.{cont}"
         return matricula
 
@@ -86,8 +85,38 @@ class Biblioteca:
         self.membro = ''
         self.livros = []
 
-    def inserirCliente (self, nome, dataCompleta, email, senha):
-        self.membro = (Cliente (nome, dataCompleta, email, senha))
+    def inserirCliente (self):
+        print ()
+        nome = input ("Digite seu nome completo: \n")
+        print()
+        print ("Digite sua data de nascimento:")
+        while True:
+            dataCompleta = input ("(Apenas números.) \n")
+            if not dataCompleta.isdigit():
+                continue
+            else:
+                break
+        print ()
+        email = input ("Digite seu e-mail: \n")
+        print ()
+        print ("Sua senha deve ter ao menos 8 caracteres, 1 número, 1 caractere maiúsculo e 1 caractere especial.")
+        while True:
+            senha = input ("Digite sua senha: \n")
+            if len (senha) < 7:
+                print ("A senha deve ter ao menos 8 caracteres:")
+            elif senha.islower():
+                print ("A senha deve ter ao menos 1 caractere maiúsculo:")
+            elif senha.isalpha():
+                print ("A senha deve ter ao menos 1 número:")
+            elif senha.isalnum():
+                print ("A senha deve ter ao menos 1 caractere especial:")
+            else:
+                break
+        sleep (2)
+        print ()
+        print ("Cadastro realizado com sucesso!")
+        print ()
+        self.membro = (Cliente (nome.title(), dataCompleta, email, senha))
         cursor.execute (f"INSERT INTO cadastro (nome, idade, matricula, email, senha) VALUES (?, ?, ?, ?, ?)", (nome, self.membro.idade, self.membro.matricula, email, senha))
         conexao.commit ()
         print (f"Membro: {self.membro.nome} \nData de nascimento: {self.membro.data} \nIdade: {self.membro.idade} \nMatrícula: {self.membro.matricula} \nE-mail: {self.membro.email} \nSenha: {len (self.membro.senha) * '*'}")
@@ -114,6 +143,7 @@ class Biblioteca:
                 continue
             
     def recuperarSenha (self):
+        print ()
         matricula_esqueceu = input ("Digite sua matrícula: \n")
         email_esqueceu = input ("Digite seu e-mail: \n")
 
@@ -126,18 +156,49 @@ class Biblioteca:
             pass
                                     
         if matricula_esqueceu in matricula_for and email_esqueceu in email_for:
-            cursor.execute (f"SELECT senha FROM cadastro WHERE senha = '{matricula_esqueceu}'")
-            for i in cursor.fetchall():
-                print (i)
+            while True:
+                print ()
+                senha_nova = input ("Digite sua nova senha: \n")
+                if len (senha_nova) < 7:
+                    print ("A senha deve ter ao menos 8 caracteres:")
+                elif senha_nova.islower():
+                    print ("A senha deve ter ao menos 1 caractere maiúsculo:")
+                elif senha_nova.isalpha():
+                    print ("A senha deve ter ao menos 1 número:")
+                elif senha_nova.isalnum():
+                    print ("A senha deve ter ao menos 1 caractere especial:")
+                else:
+                    break
+            cursor.execute (f"UPDATE cadastro SET senha = ? WHERE matricula = '{matricula_esqueceu}'", (senha_nova,))
+            conexao.commit ()
+            print ()
+            sleep (1)
+            print ("Sua senha foi alterada com sucesso!")
 
         else:
             print ("Não deu bom")
 
-    def inserirLivros (self, titulo, autor, quantidade_disponivel, genero, faixa_etaria, npaginas, data_de_edicao):
-        self.livros.append (Livro (titulo, autor, quantidade_disponivel, genero, faixa_etaria, npaginas, data_de_edicao))
+    def inserirLivros (self):
+        print ()
+        titulo = input ("Digite o título do livro: \n")
+        autor = input ("Digite o nome do autor: \n")
+        quantidade_disponivel = int (input ("Quantidade de exemplares disponíveis: \n"))
+        genero = input ("Á que gênero pertecence o seu livro: \n")
+        faixa_etaria = input ("Faixa etária do livro: \n")
+        npaginas = int (input ("Número de páginas: \n"))
+        data_de_edicao = input ("Data de publicação dessa edição: \n")
+        print ()
+
+        self.livros.append (Livro (titulo.title(), autor, quantidade_disponivel, genero, faixa_etaria, npaginas, data_de_edicao))
         cursor.execute (f"INSERT INTO livros_na_estante (titulo, autor, quantidade_disponivel, genero, faixa_etaria, npaginas, data_de_edicao) VALUES (?, ?, ?, ?, ?, ?, ?)", (titulo, autor, quantidade_disponivel, genero, faixa_etaria, npaginas, data_de_edicao))
         conexao.commit ()
+        sleep (2)
+        print ()
+        print ("Cadastro realizado com sucesso!")
+        print ()
+
     def printarLivros (self):
+        print ()
         cursor.execute (f"SELECT * FROM livros_na_estante WHERE quantidade_disponivel > 0")
         print ("Livros disponíveis:")
         print()
@@ -161,9 +222,10 @@ class Biblioteca:
                     cursor.execute (f"INSERT INTO livros_emprestados (matricula, cliente, titulo) VALUES (?,?,?)", (matricula_verifica, nomeVerifica.title(), livro_emprestado))
                     conexao.commit ()
                     print ()
-                    print (f"Livro {livro_emprestado} emprestado para você.")
+                    print (f'Livro "{livro_emprestado}" emprestado para você.')
 
     def verLivroEmprestado (self):
+        print()
         while True:
             matricula_verificarLivro = input ("Digite sua matricula novamente: \n")
 
@@ -177,23 +239,77 @@ class Biblioteca:
                 print ()
                 if len (verificar) != 0:
                     cursor.execute (f"SELECT * FROM livros_emprestados WHERE matricula = '{matricula_verificarLivro}'")
+                    sleep (1)
                     print ("Livros emprestados para você:")
+                    sleep (2)
                     for i in cursor.fetchall():
                         print(f"- {i [2]}")
                     break
                 else:
+                    sleep (1)
                     print ("Nenhum livro emprestado registrado.")
+                    sleep (2)
                     break
             else:
                 print ("Matricula incorreta.")
                 continue
 
-'''Verificação se tem livros para emrestar
-Se tiver: diminuir 1 de livro disponivel
-se nao: não é possivel emprestar'''
+    def devolverLivro (self):
+        print()
+        while True:
+            matricula_verificarLivro = input ("Digite sua matricula novamente: \n")
 
-'''Inserir na tabela de livros emprestados o cliente que pegou e o titulo do livro
+            cursor.execute (f"SELECT matricula FROM livros_emprestados WHERE matricula = '{matricula_verificarLivro}'")
+            for matricula_for in cursor.fetchall():
+                pass
 
-Depois fazer uma função para devolver o livro:
-deletando o livro da tabela livros emprestados com select e where matricula
-'''
+            if len (matricula_verificarLivro) == 9:
+                cursor.execute (f"SELECT * FROM livros_emprestados WHERE matricula = '{matricula_verificarLivro}'")
+                verificar = cursor.fetchall()
+                print ()
+                if len (verificar) != 0:
+                    cursor.execute (f"SELECT * FROM livros_emprestados WHERE matricula = '{matricula_verificarLivro}'")
+                    sleep (1)
+                    print ("Livros emprestados para você:")
+                    sleep (2)
+                    for i in cursor.fetchall():
+                        print(f"- {i [2]}")
+                    print()
+                    livro_devolver = input ("Qual destes acima desejas devolver? \n")
+                    cursor.execute (f"DELETE FROM livros_emprestados WHERE titulo = '{livro_devolver}'")
+                    conexao.commit ()
+                    cursor.execute (f"SELECT titulo, quantidade_disponivel, autor FROM livros_na_estante WHERE titulo = '{livro_devolver}'")
+                    for i in cursor.fetchall():
+                        titulo, y, autor = i
+                        cursor.execute (f"UPDATE livros_na_estante SET quantidade_disponivel = ? WHERE titulo = '{titulo}' and autor = '{autor}'", (y + 1,))
+                        conexao.commit ()
+                    print ()
+                    print (f'Livro "{livro_devolver}" devolvido á estante.')
+                    break
+                else:
+                    sleep (1)
+                    print ("Nenhum livro emprestado registrado.")
+                    sleep (2)
+                    break
+            else:
+                print ("Matricula incorreta.")
+                continue
+
+    def deletarConta (self):
+        print ()
+        matricula_deletar = input ("Digite sua matrícula novamente: \n")
+        senha_deletar = input ("Digite sua senha novamente: \n")
+
+        cursor.execute (f"SELECT matricula, senha FROM cadastro WHERE matricula = '{matricula_deletar}'")
+        for i in cursor.fetchall ():
+            if matricula_deletar in i and senha_deletar in i:
+                cursor.execute (f"SELECT * FROM livros_emprestados WHERE matricula = '{matricula_deletar}'")
+                verificar_livro = cursor.fetchall ()
+                if len (verificar_livro) != 0:
+                    print ("Você ainda tem livros á serem devolvidos!")
+                else:
+                    cursor.execute (f"DELETE FROM cadastro WHERE matricula = {matricula_deletar}")
+                    conexao.commit ()
+                    print ("Sua conta foi delatada!")
+            else:
+                print ("Cadastro não encontrado.")
